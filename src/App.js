9b204659249f46006d00398797c39f956e131e8e
Darylcosm0 <mutaidaryl@gmail.com>
2023-10-31 11:13:43 +0300

    :sparkles: Add state management with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+  const url = ''
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      // console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+    </>
   );
 }
 
